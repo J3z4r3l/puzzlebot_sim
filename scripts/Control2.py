f67a5496250a9584fc0ca63cbfc6a6b27310b357
J3z4r3l <A01735226@itesm.mx>
2024-04-19 23:47:15 -0600

    traka

--- scripts/Control2.py
+++ scripts/Control2.py
@@ -9,14 +9,14 @@ from nav_msgs.msg import Odometry
 class Controller:
     def __init__(self):
          # Configurar parametros del controlador PID para la distancia
-        self.kp_dist = 0.5  # Constante proporcional para el control de la distancia
-        self.ki_dist = 0.1  # Constante integral para el control de la distancia
-        self.kd_dist = 0.1  # Constante derivativa para el control de la distancia
+        self.kp_dist = 0.09  # Constante proporcional para el control de la distancia
+        self.ki_dist = 0.0 # Constante integral para el control de la distancia
+        self.kd_dist = 0.0  # Constante derivativa para el control de la distancia
 
         # Configurar parametros del controlador PID para el angulo
-        self.kp_ang = 0.5   # Constante proporcional para el control angular
-        self.ki_ang = 0.1   # Constante integral para el control angular
-        self.kd_ang = 0.1   # Constante derivativa para el control angular
+        self.kp_ang = 0.2   # Constante proporcional para el control angular
+        self.ki_ang = 0.03   # Constante integral para el control angular
+        self.kd_ang = 0.00   # Constante derivativa para el control angular
 
         self.integral_dist=0.0
         self.integral_ang=0.0
@@ -28,8 +28,8 @@ class Controller:
         self.y = 0.0
         self.ori_w = 0.0
         self.index = 0
-        self.x_list = [1, 2, 3, 4, 0]
-        self.y_list = [0, 0, 0, 0, 0]
+        self.x_list = [1, 1, 0, 0, 0]
+        self.y_list = [0, 1, 1, 0, 0]
         self.index = 0
         self.first=True
 
@@ -37,6 +37,7 @@ class Controller:
         self.error_ang=0.0
         self.velocidad_l=0
         self.velocidad_ang=0
+        self.velocidad_a=0
         
 
         # Inicializar nodos
@@ -49,7 +50,7 @@ class Controller:
     def odom_callback(self, data):
         self.x = data.pose.pose.position.x
         self.y = data.pose.pose.position.y
-        self.ori_w = self.wrap_to_Pi(data.pose.pose.orientation.w)
+        self.ori_w = self.wrap_to_Pi(data.pose.pose.orientation.z)
 
     def wrap_to_Pi(self,theta):
         result = np.fmod((theta + np.pi),(2 * np.pi))
@@ -68,46 +69,52 @@ class Controller:
                 self.current_time = rospy.get_time() 
                 dt = (self.current_time - self.previous_time)
                 self.previous_time = self.current_time
-            
-                self.error_dist = np.sqrt(np.square(self.y_list[self.index] - self.y) + np.square(self.x_list[self.index] - self.x))
-                self.error_ang = self.wrap_to_Pi(np.arctan2(self.y_list[self.index] - self.y, self.x_list[self.index] - self.x) - self.ori_w)
 
-                 # Controlador PID para la distancia
+                self.error_ang = np.arctan2(self.y_list[self.index]-self.y, self.x_list[self.index]-self.x) - (self.ori_w)
+                self.error_dist = np.sqrt(np.square(self.x_list[self.index]-self.x) + np.square(self.y_list[self.index]-self.y))
+
+                # Controlador PID para la distancia
                 self.integral_dist += self.error_dist * dt
                 derivative_dist = (self.error_dist - self.prev_error_dist) / dt
                 self.velocidad_l = self.kp_dist * self.error_dist + self.ki_dist * self.integral_dist + self.kd_dist * derivative_dist
                 self.prev_error_dist = self.error_dist
 
-                # Controlador PID para el angulo
+                # Controlador PID para el ang
                 self.integral_ang += self.error_ang * dt
                 derivative_ang = (self.error_ang - self.prev_error_ang) / dt
-                self.velocidad_l = self.kp_ang * self.error_ang + self.ki_ang * self.integral_ang + self.kd_ang * derivative_ang
+
+                # Si el error angular es grande, calcula la velocidad angular con el controlador PID
+                if abs(self.error_ang) > 0.6:
+                    self.velocidad_a = self.kp_ang * self.error_ang + self.ki_ang * self.integral_ang + self.kd_ang * derivative_ang
+                if abs(self.error_ang) < 0.6:
+                    # Si el error angular es pequeno, establece la velocidad angular en cero
+                    self.velocidad_a = 0
+                    self.error_ang = 0
+
                 self.prev_error_ang = self.error_ang
 
-                # Pausa para mantener la frecuencia de publicacion
-                if self.error_dist < 0.15:
-                    self.error_dist=0
-                if self.error_ang < 0.15 and self.error_ang > -0.15:
-                    self.error_ang=0
-                if self.error_ang==0 and self.error_dist==0 and self.index<len(self.x_list)-1:
-                    #self.index+=1
-                    self.index+=1
-                    
-                if self.index==len(self.x_list)-1:
-                    self.error_ed=0
-                    self.error_eang=0
-                    self.velocidad_l=0
-                    self.velocidad_ang=0
+                # Pausa para mantener la frecuencia de publica
+                if self.error_dist < 0.12:
+                    self.error_dist = 0
+
+                if abs(self.error_ang) < 0.5 and self.error_dist == 0 and self.index < len(self.x_list)-1:
+                    self.index += 1
+
+                if self.index == len(self.x_list)-1:
+                    self.error_dist = 0
+                    self.error_ang = 0
+                    self.velocidad_l = 0
+                    self.velocidad_a = 0
                 else:
-    #Publicar las velocidades del /cmd_vel solo si no hemos alcanzado el final de la lista
-                    self.msg.angular.z = self.velocidad_ang
+                    # Publicar las velocidades del /cmd_vel solo si no hemos alcanzado el final de la lista
+                    self.msg.angular.z = self.velocidad_a
                     self.msg.linear.x = self.velocidad_l
-                    #rospy.loginfo(self.index)
-                    
-                    print_info = "%3f | %3f  " %(self.index,self.error_dist)
+
+                    print_info = "%3f | %3f | %3f | %3f " %(self.index, self.velocidad_l, self.error_ang, self.error_dist)
                     rospy.loginfo(print_info)
                     self.pose_pub.publish(self.msg)
                     self.rate.sleep()
+
 
 
 if __name__== "__main__":
