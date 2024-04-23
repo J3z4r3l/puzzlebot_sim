36bc47819e94c47de5aaf1fb69cd34f29e2ee6fc
J3z4r3l <A01735226@itesm.mx>
2024-04-22 23:56:14 -0600

    commit

---
+++ scripts/Control_lazo_abierto.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+import numpy as np
+
+class SquareTrajectory:
+    def __init__(self):
+        rospy.init_node('square_trajectory_node')
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.rate = rospy.Rate(10)  # 10 Hz
+
+    def move(self, linear_speed, angular_speed, distance):
+        # Calcular el tiempo necesario para moverse a la distancia especificada
+        time_to_move = distance / linear_speed
+
+        twist = Twist()
+        twist.linear.x = linear_speed
+
+        start_time = rospy.Time.now()
+        while (rospy.Time.now() - start_time).to_sec() < time_to_move:
+            self.cmd_vel_pub.publish(twist)
+            self.rate.sleep()
+
+        # Detener el movimiento
+        twist.linear.x = 0.0
+        self.cmd_vel_pub.publish(twist)
+        self.rate.sleep()
+
+        angular_distance = np.pi / 2
+        
+        if angular_speed != 0:
+            time_to_rotate = angular_distance / angular_speed
+        else:
+            time_to_rotate = 0  
+        # Crear mensaje Twist para rotar
+        twist.angular.z = angular_speed
+
+        # Publicar el mensaje para rotar
+        start_time = rospy.Time.now()
+        while (rospy.Time.now() - start_time).to_sec() < time_to_rotate:
+            self.cmd_vel_pub.publish(twist)
+            self.rate.sleep()
+
+        twist.angular.z = 0.0
+        self.cmd_vel_pub.publish(twist)
+        self.rate.sleep()
+
+    def move_ang(self, angular_speed):
+        twist = Twist()
+        angular_distance = np.pi / 4
+        
+        time_to_rotate = angular_distance / angular_speed
+
+        # Crear mensaje Twist para rotar
+        twist.angular.z = angular_speed
+
+        # Publicar el mensaje para rotar
+        start_time = rospy.Time.now()
+        while (rospy.Time.now() - start_time).to_sec() < time_to_rotate:
+            self.cmd_vel_pub.publish(twist)
+            self.rate.sleep()
+
+        twist.angular.z = 0.0
+        self.cmd_vel_pub.publish(twist)
+        self.rate.sleep()
+
+
+    def run(self):
+        # Mover al punto (1,0)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+
+        # Mover al punto (1,1)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+#
+        ## Mover al punto (0,1)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+#
+        ## Mover al punto (0,0)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+        #
+        ###ZIGZAG o diamante
+        self.move_ang(angular_speed=0.2)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+        
+        self.move_ang(angular_speed=0.2)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+
+        self.move_ang(angular_speed=0.2)
+        self.move(linear_speed=0.2, angular_speed=0.2, distance=1)
+        self.move(linear_speed=0.2, angular_speed=0.0, distance=1)
+        
+
+
+
+        
+
+if __name__ == '__main__':
+    try:
+        square_trajectory = SquareTrajectory()
+        square_trajectory.run()
+    except rospy.ROSInterruptException:
+        pass
