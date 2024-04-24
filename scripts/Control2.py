118c2e10a1f985f4f77ed7bdc9b4dd930a4aca49
J3z4r3l <A01735226@itesm.mx>
2024-04-23 21:26:02 -0600

    V2

--- scripts/Control2.py
+++ scripts/Control2.py
@@ -8,37 +8,35 @@ from nav_msgs.msg import Odometry
 
 class Controller:
     def __init__(self):
-         # Configurar parametros del controlador PID para la distancia
-        self.kp_dist = 0.09  # Constante proporcional para el control de la distancia
-        self.ki_dist = 0.0 # Constante integral para el control de la distancia
-        self.kd_dist = 0.0  # Constante derivativa para el control de la distancia
-
-        # Configurar parametros del controlador PID para el angulo
-        self.kp_ang = 0.2   # Constante proporcional para el control angular
-        self.ki_ang = 0.03   # Constante integral para el control angular
-        self.kd_ang = 0.00   # Constante derivativa para el control angular
-
+         # PID distancia
+        self.kp_dist = 0.09  
+        self.ki_dist = 0.0
+        self.kd_dist = 0.0  
+        
+        self.error_dist=0.0
         self.integral_dist=0.0
-        self.integral_ang=0.0
         self.prev_error_dist=0.0
+        self.velocidad_l=0
+
+        # PID angulo
+        self.kp_ang = 0.2   
+        self.ki_ang = 0.03  
+        self.kd_ang = 0.00  
+
+        self.error_ang=0.0
+        self.velocidad_a=0
+        self.integral_ang=0.0
         self.prev_error_ang=0.0
 
         # Variables de estado
         self.x = 0.0
         self.y = 0.0
         self.ori_w = 0.0
+        #Lista de puntos 
         self.index = 0
         self.x_list = [1, 1, 0, 0, 0]
         self.y_list = [0, 1, 1, 0, 0]
-        self.index = 0
         self.first=True
-
-        self.error_dist=0.0
-        self.error_ang=0.0
-        self.velocidad_l=0
-        self.velocidad_ang=0
-        self.velocidad_a=0
-        
 
         # Inicializar nodos
         rospy.init_node("controller")
@@ -47,6 +45,7 @@ class Controller:
         self.msg = Twist()
         self.rate = rospy.Rate(10)
 
+    
     def odom_callback(self, data):
         self.x = data.pose.pose.position.x
         self.y = data.pose.pose.position.y
@@ -57,6 +56,23 @@ class Controller:
         if(result < 0):
                 result += 2 * np.pi
         return result - np.pi
+    
+    def error_a_l(self,index):
+        error_ang = np.arctan2(self.y_list[index] - self.y, self.x_list[index] - self.x) - self.ori_w
+        error_dist = np.sqrt((self.x_list[index] - self.x) ** 2 + (self.y_list[index] - self.y) ** 2)
+        return error_ang, error_dist
+
+   
+    def pid_controller(self,dt,error_ang,error_dist,prev_error_dist,prev_error_ang): 
+        # Controlador PID para la distancia
+        self.integral_dist += error_dist * dt
+        derivative_dist = (error_dist - prev_error_dist) / dt
+        self.velocidad_l = self.kp_dist * self.error_dist + self.ki_dist * self.integral_dist + self.kd_dist * derivative_dist
+        # Controlador PID para el ang
+        self.integral_ang += error_ang * dt
+        derivative_ang = (error_ang - prev_error_ang) / dt
+        self.velocidad_a = self.kp_ang * error_ang + self.ki_ang * self.integral_ang + self.kd_ang * derivative_ang
+
 
 
     def run(self):
@@ -67,31 +83,19 @@ class Controller:
                 self.first = False
             else:
                 self.current_time = rospy.get_time() 
-                dt = (self.current_time - self.previous_time)
+                dt = (self.current_time - self.previous_time) #get dt
                 self.previous_time = self.current_time
-
-                self.error_ang = np.arctan2(self.y_list[self.index]-self.y, self.x_list[self.index]-self.x) - (self.ori_w)
-                self.error_dist = np.sqrt(np.square(self.x_list[self.index]-self.x) + np.square(self.y_list[self.index]-self.y))
-
-                # Controlador PID para la distancia
-                self.integral_dist += self.error_dist * dt
-                derivative_dist = (self.error_dist - self.prev_error_dist) / dt
-                self.velocidad_l = self.kp_dist * self.error_dist + self.ki_dist * self.integral_dist + self.kd_dist * derivative_dist
+                self.error_ang, self.error_dist = self.error(self.index)
+               
+                # PID controller 
+                self.velocidad_l, self.velocidad_a = self.pid_controller(self.error_ang, self.error_dist,self.prev_error_dist,self.prev_error_ang)
                 self.prev_error_dist = self.error_dist
-
-                # Controlador PID para el ang
-                self.integral_ang += self.error_ang * dt
-                derivative_ang = (self.error_ang - self.prev_error_ang) / dt
-
-                # Si el error angular es grande, calcula la velocidad angular con el controlador PID
-                if abs(self.error_ang) > 0.6:
-                    self.velocidad_a = self.kp_ang * self.error_ang + self.ki_ang * self.integral_ang + self.kd_ang * derivative_ang
+                self.prev_error_ang = self.error_ang
+                
                 if abs(self.error_ang) < 0.6:
                     # Si el error angular es pequeno, establece la velocidad angular en cero
                     self.velocidad_a = 0
                     self.error_ang = 0
-
-                self.prev_error_ang = self.error_ang
 
                 # Pausa para mantener la frecuencia de publica
                 if self.error_dist < 0.12:
